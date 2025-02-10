cc3a260443b6312abc8e1801f06e9fb9351af8f6
devbelek <belekasrarov10@gmail.com>
2025-02-10 21:53:26 +0600

    env

--- playstation_backend/settings.py
+++ playstation_backend/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_ROOT = '/app/static/'
 STATIC_URL = 'static/'
 
 # Default primary key field type
