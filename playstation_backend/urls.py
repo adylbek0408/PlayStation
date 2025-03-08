f2ceae22f9e457ee5f1669d0dba678683e1521d8
devbelek <belekasrarov10@gmail.com>
2025-03-09 01:46:43 +0600

    urls

--- playstation_backend/urls.py
+++ playstation_backend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.views.decorators.csrf import csrf_exempt
+from subscriptions.views import payment_success, payment_fail
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,6 +19,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('subscriptions.urls')),
+
+    path('payment/success/', payment_success, name='direct_payment_success'),
+    path('payment/fail/', payment_fail, name='direct_payment_fail'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
 ]
